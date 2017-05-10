34038318f9bffe097e5b119ec27a3eb568df3704
SamuelSM <samus200@hotmail.es>
2017-05-10 11:54:33 +0200

    Hice un metodo de DNI.java

--- Boletin17-JUnit/src/Bolet17/DNI.java
+++ Boletin17-JUnit/src/Bolet17/DNI.java
@@ -1,12 +1,36 @@
 package Bolet17;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class DNI {
-	private char [] letra = {'q','w', 'e', 'r', 't','y','u','i','o','a','s','d','f',
-			'g','h','j','k','l','z','x','c', 'v', 'b', 'n', 'm'};
-	
-	public boolean eValido(string numeroLetra){
-		String letraA ="";
-	
+	public boolean eValido(String numeroLetra) {
+	    boolean correcto = false;
+
+	    Pattern pattern = Pattern.compile("(\\d{1,8})([TRWAGMYFPDXBNJZSQVHLCKEtrwagmyfpdxbnjzsqvhlcke])");
+
+	    Matcher matcher = pattern.matcher(numeroLetra);
+
+	    if (matcher.matches()) {
+	        String letra = matcher.group(2);
+	        String letras = "TRWAGMYFPDXBNJZSQVHLCKE";
+
+	        int index = Integer.parseInt(matcher.group(1));
+
+	        index = index % 23;
+	        String reference = letras.substring(index, index + 1);
+
+	        if (reference.equalsIgnoreCase(letra)) {
+	            correcto = true;
+	        } 
+	        else {
+	            correcto = false;
+	        }
+	    } 
+	    else {
+	        correcto = false;
+	    }
+	    return correcto;
 	}
 	public int calculaLetra(string numero){
 
