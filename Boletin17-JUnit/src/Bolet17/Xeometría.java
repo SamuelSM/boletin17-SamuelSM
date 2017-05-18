bbc7a0d99ed2adceabc16219cc9f6a5d0cca5b01
SamuelSM <samus200@hotmail.es>
2017-05-18 10:22:48 +0200

    "Acabado el XeometríaTest.java solo me queda el DNITest"

--- Boletin17-JUnit/src/Bolet17/Xeometría.java
+++ Boletin17-JUnit/src/Bolet17/Xeometría.java
@@ -4,26 +4,22 @@ import java.lang.Math;
 public class Xeometría {
 	public int perímetroCadrado(int lado){
 		//Hacer Cadrado
-	System.out.print("Perímetro cadrado :");
 	return lado*4;
 	}
 
 	public	int areaCadrado(int lado){ 
 		//Hacer Area Cadrado
-		System.out.print("Area cadrado: " );
 		return lado*lado;
 	}
 	public double teoremaPitagoras(int catetoA, int catetoB){
 		double hipotenusaC;
 		//Hacer teorema de Pitágoras
-		System.out.print("teorema de Pitágoras para calcular hipotenusa :");
 		return hipotenusaC=Math.sqrt((catetoA*catetoA)+(catetoB*catetoB));
 
 	}
 	public double teoremaPitagorasB(double hipotenusa, double catetoA){
 		double catetoB;
 		//Hacer teorema de Pitágoras
-		System.out.print("teorema de Pitágoras para calcular cateto B :");
 		return catetoB=Math.sqrt((hipotenusa*hipotenusa)-(catetoA*catetoA));
 	}
 }
